e1a25261026d7618cffe02ac4e0f018dc43e7295
Courtney Doherty <cdoherty@microsoft.com>
2021-11-19 09:51:22 +0000

    added tests and commented them out until speaking to R

---
+++ ContozooApi.Test/ContozooApi.Test.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using Moq;
+using Contozoo.API.Animals;
+using Contozoo.API.Interfaces;
+using Contozoo.API.Repositories;
+
+namespace ContozooApi.Test
+{
+    //public class AnimalsControllerTest
+    //{
+     //   [Fact]
+
+        /*public async void GetReturnOkResult()
+        {
+                //arrange
+                var mockRepo = new <IContozooRepository>();
+                mockRepo.Setup(n => n.GetAll()).Returns(mockRepo.GetTestAnimalItems());
+                var controller = new AnimalsController(mockRepo.Object);
+
+                //act
+                var result = controller.GetContozooAnimals();
+
+                //assert
+                var viewResult = Assert.IsType<ViewResult>(result);
+                var viewResultAnimals = Assert.IsAssignableFrom<List<ContozooAnimal>>(viewResult.ViewAnimals.Model);
+                Assert.Equal(5, viewResultAnimals.count); 
+        */
+    
+
+}
